f1a208c54082046ea0cf9b53f90195f4130fa573
vaibhavmali-git <malivaibhav.7620@gmail.com>
2025-10-28 16:31:03 +0530

    feat: setup initial routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Signup from "./pages/Auth/Signup";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import AllInvoices from "./pages/Invoices/AllInvoices";
+import CreateInvoice from "./pages/Invoices/CreateInvoice";
+import InvoiceDetail from "./pages/Invoices/InvoiceDetail";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
 
 const App = () => {
   return (
-    <div className='text-3xl'>
-      App
-    </div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          {/* Public routes  */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
 
-export default App
+          {/* protected routes  */}
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="invoices" element={<AllInvoices />} />
+            <Route path="invoices/new" element={<CreateInvoice />} />
+            <Route path="invoices/:id" element={<InvoiceDetail />} />
+            <Route path="profile" element={<ProfilePage />} />
+          </Route>
+
+          {/* Catch all route  */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+
+      <Toaster toastOptions={{ className: "", style: { fontSize: "13px" } }} />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Auth/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import DashboardLayout from '../Layout/DashboardLayout';
+
+const ProtectedRoute = ({children}) => {
+    const isAuthenticated = true;
+    const loading = false
+
+
+    if(loading){
+        return <div>Loading....</div>
+    }
+
+    if(!isAuthenticated){
+        return <Navigate to="/login" replace/>
+    }
+  return (
+    <DashboardLayout>{children ? children : <Outlet />}</DashboardLayout>
+  )
+}
+
+export default ProtectedRoute
