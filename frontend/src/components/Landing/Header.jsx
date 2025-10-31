555cd83ef52a7ceaf7bc037e23665759563d9ff0
vaibhavmali-git <malivaibhav.7620@gmail.com>
2025-10-31 20:02:04 +0530

    fix: tailwind warning in Header

--- frontend/src/components/Landing/Header.jsx
+++ frontend/src/components/Landing/Header.jsx
@@ -84,7 +84,7 @@ const Header = () => {
 
                 <Link
                   to="/signup"
-                  className="bg-gradient-to-r from-blue-950 to-blue-900 hover:bg-gray-800 text-white px-6 py-2.5 rounded-lg font-medium transition-all duration-200 hover:scale-105 hover:shadow-lg"
+                  className="bg-linear-to-r from-blue-950 to-blue-900 hover:bg-gray-800 text-white px-6 py-2.5 rounded-lg font-medium transition-all duration-200 hover:scale-105 hover:shadow-lg"
                 >
                   Signup
                 </Link>
