c049336e5c58c705c66e5f1c8e31d1e638d07176
vaibhavmali-git <malivaibhav.7620@gmail.com>
2025-11-02 13:05:30 +0530

    wip(ui): build Features component layout

--- frontend/src/components/Landing/Features.jsx
+++ frontend/src/components/Landing/Features.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { ArrowRight } from 'lucide-react'
+import { FEATURES } from '../../utils/data'
 
 const Features = () => {
   return (
-    <div>
-      Features
-    </div>
+    <section className=''>
+        <div className=''>
+            <div className=''>
+                <h2 className=''>Powerful features to run your business</h2>
+                <p className=''>Everything you need to manage you invoicing and get paid</p>
+            </div>
+
+            <div className=''>
+                {FEATURES.map((feature, index) => (<div key={index} className=''>
+                    <div className=''><feature.icon className=''/></div>
+                    <h3 className=''>{feature.title}</h3>
+                    <p className=''>{feature.description}</p>
+                    <a className='' href='#'>Learn More <ArrowRight className=''/></a>
+                </div>))}
+            </div>
+        </div>
+    </section>
   )
 }
 
---
+++ frontend/src/utils/data.js
@@ -0,0 +1,28 @@
+import { BarChart2, FileText, Mail, Sparkles } from "lucide-react";
+
+export const FEATURES = [
+  {
+    icon: Sparkles,
+    title: "AI Invoice Creation",
+    description:
+      "Paste any text, email, or receipt, and let our AI instantly generate a complete, professional invoice.",
+  },
+  {
+    icon: BarChart2,
+    title: "AI-Powered Dashboard",
+    description:
+      "Get smart, actionable insights about your business finances, generated automatically by AI.",
+  },
+  {
+    icon: Mail,
+    title: "Smart Reminders",
+    description:
+      "Automatically generate polite and effective payment reminder emails for overdue invoices.",
+  },
+  {
+    icon: FileText,
+    title: "Easy Invoice Management",
+    description:
+      "Easily manage all your invoices, track payments, and send reminders for overdue payments.",
+  },
+];
