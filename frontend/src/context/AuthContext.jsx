25ddc1de2dc82d9f8724f0e4d5cc007c45fec3e7
vaibhavmali-git <malivaibhav.7620@gmail.com>
2025-11-17 22:11:03 +0530

    feat(frontend): add AuthContext with basic auth state and provider setup

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { createContext, useContext, useState, useEffect } from "react";
 
-const AuthContext = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const AuthContext = createContext();
 
-export default AuthContext
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be valid within an AuthProvider");
+  }
+
+  return context;
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
+  const checkAuthStatus = async () => {};
+
+  const login = (userData, token) => {};
+
+  const logout = () => {};
+
+  const updateUser = (updatedUserData) => {};
+
+  const value = {
+    user,
+    loading,
+    isAuthenticated,
+    login,
+    logout,
+    updateUser,
+    checkAuthStatus,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
