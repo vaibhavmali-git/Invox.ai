e7f96dd54e6d1f39199172510b2d86879ec95fe7
vaibhavmali-git <malivaibhav.7620@gmail.com>
2025-11-17 14:16:48 +0530

    chore(frontend): set up axios instance with auth and error interceptors

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPath";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 80000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+/* request interceptor */
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+/* response interceptor */
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again later.");
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
